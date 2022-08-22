74b6fd170cb4144bf2d47ca804e27081f4800a0e
lulalumala <stephenlumala@gmail.com>
2022-08-22 11:29:22 +0300

    added color to output

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const head3 = document.getElementById("check")
 button1.addEventListener("click", function () {
     let lowCase = typeIn.value.toLowerCase()
     let splitted = lowCase.split("").reverse().join("")
-    if (splitted === typeIn.value) {
+    if (splitted === lowCase) {
         head3.innerText = "Yesss, a palindrome!"
+        head3.classList.add("trueVal")
+
         
     }
     
     else {
-        head3.innerHTML="That was not a palindrome"
+        head3.innerHTML = "That was not a palindrome"
+        head3.classList.remove("trueVal")
         head3.classList.add("falseVal")
     }
 })
